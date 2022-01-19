f3d034a9ec9fc7bdab9e081252e94d84d388ea16
hyeonukdev <lego7407@gmail.com>
2022-01-19 23:34:52 +0900

    :monocle_face: Add dummyData

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import DiaryEditor from "./pages/DiaryEditor";
 import DiaryList from "./pages/DiaryList";
-
-const dummyList = [
-  {
-    id: 1,
-    author: "tester1",
-    content: "하이1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "tester2",
-    content: "하이2",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "tester1",
-    content: "하이3",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-];
+import dummyData from "./datas/dummy.json";
 
 const App = () => {
   return (
     <div className="App">
       <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryList diaryList={dummyData} />
     </div>
   );
 };
--- src/pages/DiaryList.js
+++ src/pages/DiaryList.js
@@ -2,12 +2,13 @@ import DiaryItem from "./DiaryItem";
 import "./DiaryList.css";
 
 const DiaryList = ({ diaryList }) => {
+  console.log(diaryList.datas);
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
-      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <h4>{diaryList.datas.length}개의 일기가 있습니다.</h4>
       <div>
-        {diaryList.map((it) => (
+        {diaryList.datas.map((it) => (
           <DiaryItem key={`diaryitem_${it.id}`} {...it} />
         ))}
       </div>
