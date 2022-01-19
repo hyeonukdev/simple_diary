0759b3b3126176b153e1692e2199fa8ff8520870
hyeonukdev <lego7407@gmail.com>
2022-01-19 23:24:46 +0900

    ✨ Add DiaryList & Item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import DiaryEditor from "./pages/DiaryEditor";
+import DiaryList from "./pages/DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "tester1",
+    content: "하이1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "tester2",
+    content: "하이2",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "tester1",
+    content: "하이3",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 const App = () => {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 };
