b854b403ee8a71dc945e16d3ab85a40f7551a4cc
hyeonukdev <lego7407@gmail.com>
2022-01-21 23:20:09 +0900

    :sparkles: Add Edit function

--- src/pages/DiaryList.js
+++ src/pages/DiaryList.js
@@ -1,14 +1,14 @@
 import DiaryItem from "./DiaryItem";
 import "./DiaryList.css";
 
-const DiaryList = ({ diaryList, onDelete }) => {
+const DiaryList = ({ onEdit, onRemove, diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={`diaryitem_${it.id}`} {...it} onDelete={onDelete} />
+          <DiaryItem key={it.id} {...it} onEdit={onEdit} onRemove={onRemove} />
         ))}
       </div>
     </div>
